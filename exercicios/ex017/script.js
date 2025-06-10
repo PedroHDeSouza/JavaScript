a3afff1777a9ff9e83ef3e5889f69c7a0f359b96
Pedro Henrique de Souza <204240786+PedroHDeSouza@users.noreply.github.com>
2025-06-10 15:23:19 -0300

    ex 17

--- exercicios/ex017/script.js
+++ exercicios/ex017/script.js
@@ -1,19 +1,21 @@
 function tabuada(){
     let num = document.getElementById('txtn')
     let tab = document.getElementById('seltab')
-    if (num.value.length == 0) {
-        window.alert('Por favor, digite um número!')
-    }else {
+    if (num.value.length == 0){
+        window.alert('insira um numero')
+    }else{
         let n = Number(num.value)
         let c = 1
         tab.innerHTML = ''
-        while (c <= 10){
+        for (let c = 1; c <= 10; c++){
             let item = document.createElement('option')
-            item.text = `${n} x ${c} = ${n*c}`
+            item.text = `${n} x ${c} = ${n*c}` 
+            item.value = `tab${c}`
             tab.appendChild(item)
-            c++
-        }
+
+        }   
     }
+    
     
 
 }
