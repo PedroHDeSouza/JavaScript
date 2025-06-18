de6bcc9a9914714ea85e26dbda4e0dea7fd747a0
Pedro Henrique de Souza <204240786+PedroHDeSouza@users.noreply.github.com>
2025-06-18 15:12:14 -0300

    arrumando as pastas e começando projeto proprio

---
+++ exercicios_proprios/List/javascript.js
@@ -0,0 +1,7 @@
+var tar = document.querySelector("input#tarefa")
+var add = document.querySelector("button#adicionar")
+var list = document.querySelector("ul#lista")
+
+function adicionar(l){
+    
+}
