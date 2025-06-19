f29d8818de6451f82a3d717546fe3a3a3c8f2014
Pedro Henrique de Souza <204240786+PedroHDeSouza@users.noreply.github.com>
2025-06-19 15:25:31 -0300

    exercicio proprio

--- exercicios_proprios/List/javascript.js
+++ exercicios_proprios/List/javascript.js
@@ -1,7 +1,26 @@
-var tar = document.querySelector("input#tarefa")
-var add = document.querySelector("button#adicionar")
-var list = document.querySelector("ul#lista")
+let contador = 0;
 
-function adicionar(l){
-    
+function adicionartarefa(){
+    const input = document.getElementById("tarefa");
+    const valor = input.value.trim();
+
+    if (valor !== ""){
+        contador++; 
+
+        const li = document.createElement("li");
+
+        const checkbox = document.createElement("input");
+        checkbox.type = "checkbox"
+        checkbox.id = "tarefa-" + contador;
+
+        const label = document.createElement("label");
+        label.htmlFor = checkbox.id;
+        label.textContent = valor;
+
+        li.appendChild(checkbox);
+        li.appendChild(label);
+
+        document.getElementById("lista").appendChild(li);
+        input.value=""
+    }
 }
